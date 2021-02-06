215ac79f9d988edaa6da5b164cf75f94b61d4904
ronpichardo <ron@ronpich.com>
2021-02-05 21:24:37 -0500

    Added boilerplate to crestron_copy

--- crestron_copy.py
+++ crestron_copy.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python
+
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+def main():
+
+    module = AnsibleModule(argument_spec={})
+
+    response = { "update": "outdated" }
+    module.exit_json(changed=False, data=response)
+
+
+if __name__ == '__main__':
+    main()
